8707de73103ddcecf31460b15f48057da3c5219f
d0u9 <d0u9.su@outlook.com>
2018-04-09 16:03:41 +0800

    update mount, add udev rule

--- mount/install.sh
+++ mount/install.sh
@@ -15,3 +15,8 @@ install -o $user -g $group  tri-umount.sh "$target_dir/tri-umount"
 
 rm -fr "$target_dir/tri-mount"
 install -o $user -g $group tri-mount.sh "$target_dir/tri-mount"
+
+rm -fr /etc/udev/rules.d/90-trident.rules
+install -o root -g root -m 644 udev/90-trident.rules /etc/udev/rules.d/
+udevadm control --reload-rules
+udevadm trigger
